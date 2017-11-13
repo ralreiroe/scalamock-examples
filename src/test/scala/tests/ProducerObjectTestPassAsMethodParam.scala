b8d95b3cc48243da97a94e2afa36ca9d2fed3f28
Ralf Oenning <ralf@invicto.net>
2017-11-13 17:14:18 +0000

    test a static method - vary it via parameter

---
+++ src/test/scala/tests/ProducerObjectTestPassAsMethodParam.scala
@@ -0,0 +1,35 @@
+package tests
+
+import org.scalatest.{FlatSpec, Matchers}
+
+object ProducerObject4 {
+
+  def someMethod(implicit co: ConfigObjectsTrait4) = {
+
+    co.getString("queries.client")
+
+  }
+}
+
+import com.typesafe.config.{Config, ConfigFactory}
+
+trait ConfigObjectsTrait4 {
+
+  lazy val config: Config = ConfigFactory.load()
+
+  def getString(str: String) = config.getString(str)
+  def getObject(str: String) = config.getObject(str)
+
+}
+
+class ProducerTest4 extends FlatSpec with Matchers {
+
+  "I can test some method with a different config by passing one in" should "work" in {
+
+    implicit val co = new ConfigObjectsTrait4 {           //<========= replaceable for testing
+      override def getString(str: String): String = "blah"
+    }
+
+    ProducerObject4.someMethod shouldBe "blah"
+  }
+}
